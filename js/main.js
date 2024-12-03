1bf3fe7297560452f217a8b6f5a68b20d9843443
Katherine DiMattia <kdimat2@lsu.edu>
2024-12-02 23:09:42 -0600

    styling styling styling

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 // JS scripts placed here
+const toggleButton = document.querySelector('.toggle-button');
+const navbar = document.querySelector('.navbar ul');
+
+toggleButton.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+});
+
